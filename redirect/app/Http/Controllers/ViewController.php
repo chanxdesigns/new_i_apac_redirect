a0b126a709caa9a1ea46baa7c83452d8b2be84cb
Chanx Singha <chanx.designs@gmail.com>
2016-03-04 15:09:37 +0530

    Added method comments

--- redirect/app/Http/Controllers/ViewController.php
+++ redirect/app/Http/Controllers/ViewController.php
@@ -8,14 +8,17 @@ use App\Http\Requests;
 
 class ViewController extends Controller
 {
+    //Return the View for Completed
     public function completed ($respid) {
         return view('completed', compact('respid'));
     }
 
+    //Return the View for Terminated
     public function terminated ($respid) {
         return view('terminated', compact('respid'));
     }
 
+    //Return the View for Quotafull
     public function quotafull ($respid) {
         return view('quotafull', compact('respid'));
     }
