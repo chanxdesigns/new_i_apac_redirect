52fe5d55cf60f551044a0b73078db4452a25f091
Chanx Singha <chanx.designs@gmail.com>
2016-03-04 15:07:48 +0530

    Initial Commit

--- redirect/resources/views/quotafull.blade.php
+++ redirect/resources/views/quotafull.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-        <html lang="en">
+<html lang="en">
 <head>
-    <title>{{$respid}} - Completed</title>
+    <title>{{$respid}} - Quotafull</title>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,900,700' rel='stylesheet' type='text/css'>
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
 <div class="notifier">
     <h1><span>User ID:</span> {{$respid}}</h1>
-    <p>Thanks for Completing the Survey Successfully</p>
+    <p>Thanks for Participating but unfortunately your Quota has been filled.</p>
 </div>
 </body>
 </html>
--- redirect/resources/views/terminated.blade.php
+++ redirect/resources/views/terminated.blade.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-        <html lang="en">
+<html lang="en">
 <head>
-    <title>{{$respid}} - Completed</title>
+    <title>{{$respid}} - Terminated</title>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,100,300,900,700' rel='stylesheet' type='text/css'>
     <link href="{{asset('css/app.css')}}" rel="stylesheet">
 </head>
 <body>
 <div class="notifier">
     <h1><span>User ID:</span> {{$respid}}</h1>
-    <p>Thanks for Completing the Survey Successfully</p>
+    <p>Thanks for Participating, but You are not Eligible.</p>
 </div>
 </body>
 </html>
