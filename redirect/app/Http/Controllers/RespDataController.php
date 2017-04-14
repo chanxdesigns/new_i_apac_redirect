8479b8e2f65bf50f06631f8c4636227b64ef0f1e
Chandra Kumar Singha <chanx.designs@gmail.com>
2017-04-14 12:23:30 +0530

    Links count for link detection

--- redirect/app/Http/Controllers/RespDataController.php
+++ redirect/app/Http/Controllers/RespDataController.php
@@ -102,7 +102,6 @@ class RespDataController extends Controller
     public function getLinksAndAbout () {
         //Get Links From DB
         $links = DB::table('projects_list')->select('C_Link','T_Link','Q_Link','About')->where('Project ID', '=', $this->projectid)->where('Vendor', '=', $this->vendor)->where('Country','=', $this->country)->get();
-        print_r($links);
         if (count($links)) {
         $this->t_link = $links[0]->T_Link;
         $this->c_link = $links[0]->C_Link;
